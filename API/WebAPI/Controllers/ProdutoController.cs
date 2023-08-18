31afea7fc9330ecc386eefc765f9dcb107335694
aluno <Aluno@PR02-302-18.CSED.LAB>
2023-08-18 11:30:06 -0300

    Inicio da API

---
+++ API/WebAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/produto")]
+public class ProdutoController : ControllerBase
+{
+    private List<Produto> produtos = new List<Produto>();
+    //GET: api/produto/listar
+    [HttpGet]
+    [Route("listar")]
+    public string Listar()
+    {
+        return "Hello World de uma API em C# com Watch";
+    }
+
+    [HttpPost]
+    [Route("cadastrar")]
+    public Produto Cadastrar(Produto produto)
+    {
+        return produto;
+    }
+}
